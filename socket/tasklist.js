ce7fef4c032459e46bd220cc8ce119ff226e8c64
ArisaSky <sky0333@gmail.com>
2018-01-05 14:26:38 +0800

    Convert socket to ES6 syntax

--- socket/tasklist.js
+++ socket/tasklist.js
@@ -1,28 +1,28 @@
 // express
 const express = require('express');
 const router = express.Router();
-var returnRouter = function(io) {
+var returnRouter = (io) => {
   // socket
   let totalConnections = 0;
-  io.on('connection', function(socket) {
+  io.on('connection', (socket) => {
     totalConnections++;
     console.log('Socket connection initialized. Currently (' + totalConnections + ') Connected.');
-    socket.on('disconnect', function(res) {
+    socket.on('disconnect', (res) => {
       totalConnections--;
       console.log('Socket connection closed. Currently (' + totalConnections + ') Connected.');
     });
 
-    socket.on('save-tasklist', function (res) {
+    socket.on('save-tasklist', (res) => {
       console.log(res);
       io.emit('new-tasklist',  res);
     });
 
-    socket.on('delete-tasklist', function (res) {
+    socket.on('delete-tasklist', (res) => {
       console.log(res);
       io.emit('remove-tasklist',  res);
     });
 
-    socket.on('edit-tasklist', function (res) {
+    socket.on('edit-tasklist', (res) => {
       console.log(res);
       io.emit('update-tasklist',  res);
     });
